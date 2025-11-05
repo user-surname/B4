2c7bab24c9accc73b22167c2e7d7637c48b7d7b2
Marcelo <backlogbcn@gmail.com>
2025-11-05 17:10:53 +0100

    v0.20251105

---
+++ B4.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+///
+
+namespace B4.Api.Controllers
+{
+    /// <summary>
+    ///    Controlador de test: Weatherforecast
+    /// </summary>
+    [Route("[controller]")] // Define la ruta base como "WeatherForecast"
+    public class WeatherForecastController : ControllerBase
+    {
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="Date"></param>
+        /// <param name="TemperatureC"></param>
+        /// <param name="Summary"></param>
+        public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+        {
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+
+        /// <summary>
+        ///     Get /weatherforecast
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet] // Define el método HTTP para GET
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                   new WeatherForecast
+                   (
+                       DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                       Random.Shared.Next(-20, 55),
+                       summaries[Random.Shared.Next(summaries.Length)]
+                   ))
+                   .ToArray();
+            return forecast;
+        }
+    }
+}
